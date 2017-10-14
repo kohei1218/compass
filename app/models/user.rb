f07678bec728eb1d2053c68af1a57610764bfb08
KUBOKAZUYA <jpkk623@gmail.com>
2017-10-14 19:29:57 +0900

    Add devise & each models

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :job_applications
+  has_many :applied_recruits, class_name: "Recruit", through: :job_applications
+  has_many :favorites
+  has_many :favorite_recruits, class_name: "Recruit", through: :favorites
+  has_many :view_logs
+  has_many :viewed_recruits, class_name: "Recruit", through: :view_logs
+end
