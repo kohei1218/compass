0a651d1239108cfccacca8deef6a934a56a4b6f0
kohei1218 <yandapanda1218@gmail.com>
2017-11-03 22:28:49 +0900

    募集一覧view仕事内容まで追加

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -3,6 +3,7 @@ class Recruit < ApplicationRecord
   has_many :applied_users, class_name: "User", through: :job_applications
   has_many :favorites
   has_many :favorite_users, class_name: "User", through: :favorites
+  belongs_to :company
 
   validates :occupation, presence: true
 end
