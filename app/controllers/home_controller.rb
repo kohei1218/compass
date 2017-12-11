79253634f1b32ca88948469315312b5461466956
kohei1218 <yandapanda1218@gmail.com>
2017-12-11 20:11:49 +0900

    add top page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   layout 'user'
 
   def index
+    @q = Recruit.search(params[:q])
+    @company_count = Company.count
+    @recruit_count = Recruit.count
+    @recruits = @recruits = @q.result(distinct: true).page(params[:page]).per(15).order("updated_at DESC")
   end
 end
